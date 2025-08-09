d2fc8bb2c076bd091e0f76c9fc31bb9514ba8fd5
Aditya Pachupate <adityapachupate@gmail.com>
2025-08-09 12:00:31 +0530

    remove Id from UpdateRestaurantCommand

--- Restaurants.Applications/Restaurants/Commands/UpdateRestaurant/UpdateRestaurantCommand.cs
+++ Restaurants.Applications/Restaurants/Commands/UpdateRestaurant/UpdateRestaurantCommand.cs
@@ -9,7 +9,7 @@ namespace Restaurants.Applications.Restaurants.Commands.UpdateRestaurant
 {
     public class UpdateRestaurantCommand : IRequest<bool>
     {
-        public int Id { get; set; }
+        
         public string Name { get; set; }=default!;
         public string Description { get; set; }= default!;
         public bool HasDelivery { get; set; }
