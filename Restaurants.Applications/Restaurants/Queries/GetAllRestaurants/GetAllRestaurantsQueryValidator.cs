b2fbb4f4506abd97bb0a1ab9bc40feff72de8863
Aditya Pachupate <adityapachupate@gmail.com>
2025-09-14 19:31:58 +0530

    Add validation for GetAllRestaurantsQuery
    Introduces the GetAllRestaurantsQueryValidator class, which
    validates the pageNumber and pageSize properties of the
    GetAllRestaurantsQuery object using FluentValidation.
    Ensures pageNumber is greater than 1 and pageSize is one
    of the allowed values: 5, 10, or 20.

---
+++ Restaurants.Applications/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Restaurants.Applications.Restaurants.Queries.GetAllRestaurants
+{
+    public class GetAllRestaurantsQueryValidator : AbstractValidator<GetAllRestaurantsQuery>
+    {
+        private int[] allowedPageSizes = [ 5, 10, 20 ];
+        public GetAllRestaurantsQueryValidator()
+        {
+            RuleFor(x => x.pageNumber)
+                .GreaterThan(1).WithMessage("Page number must be greater than 0.");
+
+            RuleFor(x => x.pageSize)
+                .Must(x => allowedPageSizes.Contains(x))
+                .WithMessage($"Page size must be one of the following values: {string.Join(", ", allowedPageSizes)}");
+        }
+    }
+}
